1a99e105e5d64a1bdab809942dc7ca752d7487f2
mohd kaif <mk2818356@gmail.com>
2026-04-28 18:23:03 +0530

    init maven

---
+++ java/maven/my-app/src/main/java/com/moviebackedn/app/App.java
@@ -0,0 +1,17 @@
+package com.moviebackedn.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
